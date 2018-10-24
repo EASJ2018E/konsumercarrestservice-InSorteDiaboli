8feaf7370a7d95f864002aa9c7b30705bd737877
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-10-24 11:08:57 +0200

    Tilføjet første axios kald (getAllCars)

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,10 +2,27 @@ import axios, {
     AxiosResponse,
     AxiosError} from "../../node_modules/axios/index";
 
-    //http://rest-pele-easj-dk.azurewebsites.net/api/Cars
-
     interface ICar {
         model:string;
         vendor:string;
         price:number;
+    }
+
+    let getAllCars:HTMLButtonElement = <HTMLButtonElement> document.getElementById("getAllButton");
+    getAllCars.addEventListener('click',showAllCars);
+
+    function showAllCars():void {
+        let uri:string = "http://rest-pele-easj-dk.azurewebsites.net/api/Cars";
+
+        axios.get<ICar[]>(uri)
+        .then(function (response:AxiosResponse<ICar[]>):void{
+
+            let result:string = "<ol>";
+            response.data.forEach((car:ICar)=>{
+                result += "<li>" + car.model + car.vendor + car.price.toString() + "</li>"
+            });
+            result += "</ol>";
+        }
+        )
+        .catch()
     }
